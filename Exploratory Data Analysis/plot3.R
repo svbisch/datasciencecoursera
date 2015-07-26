51c6a8f013946578cd72a6c77651d64927f1a3d1
Sven Bischoff <sven.bischoff@posteo.de>
2015-07-26 13:19:56 +0200

    added plot #3

---
+++ Exploratory Data Analysis/plot3.R
@@ -0,0 +1,19 @@
+library(dplyr)
+library(ggplot2)
+
+NEI <- readRDS("summarySCC_PM25.rds")
+
+# filter Baltimore City, group by year, calculate sum of emissions from all sources
+q3 <- filter(NEI, fips=="24510") %>%
+    group_by(year, type) %>% 
+    summarise(pm25_tot=sum(Emissions, na.rm=TRUE))
+
+# map year and total emssion to the axes, plot points layer, add linear regression layer, 
+# make facets by type of source
+p3 <- ggplot(q3, aes(year, pm25_tot)) +
+    geom_point() +
+    geom_smooth(method="lm", se=FALSE) +
+    facet_wrap(~type) +
+    labs(y=expression("Amount of " * PM[2.5] * " in tons")) +
+    labs(title="Baltimore City") 
+ggsave("plot3.png")
